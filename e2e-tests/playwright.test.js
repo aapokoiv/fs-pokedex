5f3c11942535e2c5154028f7cd5a69cfbd2fb73f
Aapo Koivula <koivula.aapo@gmail.com>
2026-08-03 17:38:53 +0300

    Added a new e2e test

--- e2e-tests/playwright.test.js
+++ e2e-tests/playwright.test.js
@@ -2,8 +2,16 @@ const { test, describe, expect } = require('@playwright/test')
 
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
-     await page.goto('')
+    await page.goto('')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+  test('can navigate from front page to ivysaur', async ({ page }) => {
+    await page.goto('')
+    await page.getByRole('link', { name: 'ivysaur' }).click()
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('speed')).toBeVisible()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+    await expect(page.getByText('overgrow')).toBeVisible()
   })
 })
